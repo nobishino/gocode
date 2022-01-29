ea740d5f052561808172426a22b8a20787e08c88
nobishino <nobishii11@gmail.com>
2022-01-29 15:37:59 +0900

    Refactor

--- hackasm/cmd/hackasm/main.go
+++ hackasm/cmd/hackasm/main.go
@@ -14,28 +14,33 @@ func init() {
 	flag.StringVar(&outPath, "o", "out.hack", "specify output file name")
 }
 
-func exec(srcPath, outPath string) {
+func exec(srcPath, outPath string) int {
 	f, err := os.Open(srcPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return 1
 	}
 	defer f.Close()
 
 	out, err := os.Create(outPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return 1
 	}
 	defer out.Close()
 
 	if err := hackasm.Assemble(f, out); err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return 1
 	}
 
 	log.Println("assembled into", outPath)
+	return 0
 }
 
 func main() {
 	flag.Parse()
 	srcPath := flag.Arg(0)
-	exec(srcPath, outPath)
+	stCode := exec(srcPath, outPath)
+	os.Exit(stCode)
 }
