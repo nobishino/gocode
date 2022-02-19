478bda526385190418b6a8288b37a5a529595975
nobishino <nobishii11@gmail.com>
2022-02-19 12:29:10 +0900

    Add Faling test: push constant 7

--- hackvm/codewriter/codewriter.go
+++ hackvm/codewriter/codewriter.go
@@ -5,20 +5,24 @@ import "io"
 type CodeWriter struct{}
 
 //出力ファイル/ストリームを開き書き込む準備を行う
-func New(w io.WriteCloser) *CodeWriter {
+func New(w io.Writer) *CodeWriter {
 	return new(CodeWriter)
 }
 
-// CodeWriterに、あたらしいVMふぁいるの変換が開始したことを伝える
+// CodeWriterに、あたらしいVMファイルの変換が開始したことを伝える
 func (c *CodeWriter) SetFileName(n string) {
 
 }
 
 //与えられた算術コマンドをアッセンブリーコードに変換し、それを書き込む
-func (c *CodeWriter) WriteArithmetic(command string) {}
+func (c *CodeWriter) WriteArithmetic(command string) error {
+	return nil
+}
 
 //C_PUSHまたはC_POPコマンドをアッセンブリーに変換しそれを書き込む
-func (c *CodeWriter) WritePushPop(command string, segment string, index int) {}
+func (c *CodeWriter) WritePushPop(command string, segment string, index int) error {
+	return nil
+}
 
 // 出力ファイルを閉じる
 func (c *CodeWriter) Close() error { return nil }
