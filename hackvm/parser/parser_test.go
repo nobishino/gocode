ce2660dfe4159b8003d7f4103d78d9fe3867b58e
nobishino <nobishii11@gmail.com>
2022-02-26 14:24:55 +0900

    Refactor test

--- hackvm/parser/parser_test.go
+++ hackvm/parser/parser_test.go
@@ -11,15 +11,21 @@ func TestParserArithmetic(t *testing.T) {
 	const wantType = "C_ARITHMETIC"
 	testcases := []struct {
 		in  string
-		arg string
+		arg []string
 	}{
-		{in: "add\n", arg: "add"},
-		{in: "sub\n", arg: "sub"},
+		{"add", []string{"add"}},
+		{"add\n", []string{"add"}},
+		{"sub", []string{"sub"}},
 	}
 	for _, tc := range testcases {
 		p := parser.New(strings.NewReader(tc.in))
+		var idx int
 		if !p.HasMoreCommands() {
-			t.Fatal("parser should have at least 1 command")
+			if idx < len(tc.arg) {
+				t.Fatalf("there should be %d commands, but got only %d commands",
+					len(tc.arg), idx)
+			}
+			return // has read all commands
 		}
 		p.Advance()
 		gotType := p.CommandType()
@@ -27,7 +33,7 @@ func TestParserArithmetic(t *testing.T) {
 			t.Errorf("want %q, but got %q", wantType, gotType)
 		}
 		gotArg1 := p.Arg1()
-		if gotArg1 != tc.arg {
+		if gotArg1 != tc.arg[idx] {
 			t.Errorf("want %q, but got %q", tc.arg, gotArg1)
 		}
 	}
