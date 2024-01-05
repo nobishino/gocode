2338b5c74c2b4069063d48779bea97af480e1b2e
nobishino <nobishii11@gmail.com>
2024-01-05 20:12:55 +0900

    update

--- examples/simplehttp/main.go
+++ examples/simplehttp/main.go
@@ -6,19 +6,67 @@ import (
 )
 
 func main() {
-	handler1 := func(w http.ResponseWriter, r *http.Request) {
-		log.Println("handler1が呼び出されました")
-	}
-	handler2 := func(w http.ResponseWriter, r *http.Request) {
-		log.Println("handler2が呼び出されました")
+	// runWithoutPackageVariable()
+	// runWithoutMultiplexer()
+	runWithPackageVariables()
+}
+
+// ServerとServeMuxを関数内で作るタイプの実装
+func runWithoutPackageVariable() {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", handler1)
+	serveMux.HandleFunc("/abc", handler2)
+	serveMux.HandleFunc("/favicon.ico", handlerForFavicon)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
 	}
 
-	multiplexer := http.NewServeMux()
-	multiplexer.HandleFunc("/", handler1)
-	multiplexer.HandleFunc("/abc", handler2)
+	if err := server.ListenAndServe(); err != nil {
+		// (*http.Server).ListenAndServeは常にnon-nil errorを返す。
+		log.Println(err)
+	}
+}
 
-	if err := http.ListenAndServe(":8080", multiplexer); err != nil {
+func runWithoutMultiplexer() {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(handler1),
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		// (*http.Server).ListenAndServeは常にnon-nil errorを返す。
+		log.Println(err)
+	}
+}
+
+func runWithPackageVariables() {
+	// パスが/にマッチするリクエストはhandler1にルーティングする
+	http.HandleFunc("/", handler1)
+	// パスが/abcにマッチするリクエストはhandler2にルーティングする
+	http.HandleFunc("/abc", handler2)
+	// パスが/favicon.icoにマッチするリクエストはfaviconhandlerにルーティングする
+	http.HandleFunc("/favicon.ico", handlerForFavicon)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		// http.ListenAndServeは常にnon-nil errorを返す。
 		log.Println(err)
 	}
+}
+
+func handler1(w http.ResponseWriter, r *http.Request) {
+	log.Println("handler1が呼び出されました")
+	w.Write([]byte("I'm handler 1"))
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	log.Println("handler2が呼び出されました")
+	w.Write([]byte("I'm handler 2"))
+}
+
+// Webブラウザーは/favicon.icoにリクエストを送るのでそのためのハンドラーも別に作っておく
+func handlerForFavicon(w http.ResponseWriter, r *http.Request) {
+	log.Println("favicon handlerが呼び出されました")
+	// w.Write([]byte("I'm handler 2")) TODO: gopherくん画像を返す
 }
